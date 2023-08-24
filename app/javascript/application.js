e395f1e6b309186eefa20eb78496f3c440f3549c
v-rupesh <133742376+v-rupesh@users.noreply.github.com>
2023-08-24 16:32:13 +0530

    Added all functionality to app

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -1,3 +1,30 @@
-// Configure your import map in config/importmap.rb. Read more: https://github.com/rails/importmap-rails
-import "@hotwired/turbo-rails"
+import { Turbo } from "@hotwired/turbo-rails"
 import "controllers"
+
+//= require rails-ujs
+//= require jquery
+//= require activestorage
+//= require turbolinks
+//= require semantic-ui
+//= require_tree .
+
+
+document.addEventListener("turbo:load", function() {
+  $('.ui.dropdown').dropdown();
+
+  $('.message .close').on('click', function() {
+    $(this).closest('.message').transition('fade');
+  });
+
+  $('#message_body').on('keydown', function(e) {
+    if (e.key === 'Enter') {
+      $('button[type="submit"]').click();
+      e.target.value = '';
+    }
+  });
+
+  const messagesContainer = document.getElementById('messages');
+  if (messagesContainer) {
+    messagesContainer.scrollTop = messagesContainer.scrollHeight;
+  }
+});
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_23_155402) do
+ActiveRecord::Schema[7.0].define(version: 2023_08_24_070648) do
+  create_table "messages", force: :cascade do |t|
+    t.integer "user_id"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_messages_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -18,4 +26,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_08_23_155402) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "messages", "users"
 end
